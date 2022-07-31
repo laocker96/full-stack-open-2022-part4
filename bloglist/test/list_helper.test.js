eecb22dcee5ad37dfc168d9947905533a2f02fd6
Simone Bergamin <simone.bergamin2a@gmai.com>
2022-07-31 13:40:41 +0200

    Exercise 4.4

--- bloglist/test/list_helper.test.js
+++ bloglist/test/list_helper.test.js
@@ -1,8 +1,57 @@
 const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
-  const blogs = []
+    const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    const listWithMoreThanOneBlog = [
+        {
+            _id: "5a422a851b54a676234d17f7",
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        }
+    ]
+
+    test('when list has no blogs, equals 0', () => {
+        const result = listHelper.totalLikes([])
+        expect(result).toBe('Post list is empty')
+    })
+
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        expect(result).toBe(5)
+    })
+
+    test('when list has more than one blog, equals the sum of likes in blogs', () => {
+        const result = listHelper.totalLikes(listWithMoreThanOneBlog)
+        expect(result).toBe(19)
+    })
+
+
 })
--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (posts) => {
     return 1
 }
 
+const totalLikes = (posts) => {
+    if (posts.length === 0)
+        return 'Post list is empty'
+    return posts.reduce((sum, current) => sum += current.likes, 0);
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
